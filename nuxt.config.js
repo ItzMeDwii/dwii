2973cb42422863b7a1ae38386d3c6b4a532c85db
Satya Bagus Dwiatmaja <dwiiunknown@gmail.com>
2021-04-08 04:13:47 +0000

    .

--- nuxt.config.js
+++ nuxt.config.js
@@ -23,7 +23,7 @@ export default {
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
-    '@/plugins/bootstrap.js'
+    { src: '@/plugins/bootstrap.js', mode: 'client' }
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
