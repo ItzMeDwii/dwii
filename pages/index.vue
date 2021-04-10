02cdaa1b2f28eb240eca644f5a8a2ba5c7ef461f
Satya Bagus Dwiatmaja <dwiiunknown@gmail.com>
2021-04-10 13:42:48 +0000

    add layout

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,14 @@
 <template>
   <div class="container">
-    a
+    <div class="content-1 my-5">
+      A
+    </div>
+    <div class="content-2 my-5">
+      B
+    </div>
+    <div class="content-3 my-5">
+      C
+    </div>
   </div>
 </template>
 
