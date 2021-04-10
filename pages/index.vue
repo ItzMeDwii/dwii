3cce01324e5bb9ac36b2f7596c4b3bc8580bb6e2
Satya Bagus Dwiatmaja <dwiiunknown@gmail.com>
2021-04-10 13:55:40 +0000

    add content-1

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,13 @@
 <template>
   <div class="container">
+    <div class="blank-space1" />
     <div class="content-1 my-5">
-      A
+      <h1 class="mb-1">
+        Hi! Im DwiiUnknown
+      </h1>
+      <p class="lead">
+        My name is Satya Bagus Dwiatmaja. Im Frontend Web Developer, love to make nice websites and discord bots.
+      </p>
     </div>
     <div class="content-2 my-5">
       B
@@ -9,11 +15,6 @@
     <div class="content-3 my-5">
       C
     </div>
+    <div class="blank-space1" />
   </div>
 </template>
-
-<script>
-</script>
-
-<style>
-</style>
