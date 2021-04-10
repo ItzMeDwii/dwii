ddb2584d0e823de85f34eaef74c979e3335faf86
Satya Bagus Dwiatmaja <dwiiunknown@gmail.com>
2021-04-10 14:23:23 +0000

    add grid to content-1

--- pages/index.vue
+++ pages/index.vue
@@ -2,12 +2,16 @@
   <div class="container">
     <div class="blank-space1" />
     <div class="content-1 my-5">
-      <h1 class="mb-1">
-        Hi! Im DwiiUnknown
-      </h1>
-      <p class="lead">
-        My name is Satya Bagus Dwiatmaja. Im Frontend Web Developer, love to make nice websites and discord bots.
-      </p>
+      <div class="row">
+        <div class="col-12 col-lg-7 col-md-6 col-sm-12">
+          <h1 class="mb-1">
+            Hi! Im DwiiUnknown
+          </h1>
+          <p class="lead">
+            My name is Satya Bagus Dwiatmaja. Im Frontend Web Developer, love to make nice websites and discord bots.
+          </p>
+        </div>
+      </div>
     </div>
     <div class="blank-space1" />
     <div class="content-2 my-5">
