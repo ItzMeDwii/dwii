e3e12df8898adbb37cea328e4bd58d149734a6f9
ItzMeDwii <dwiiunknown@gmail.com>
2021-04-11 13:38:50 +0700

    update layout

--- pages/index.vue
+++ pages/index.vue
@@ -1,26 +1,59 @@
 <template>
-  <div class="container">
-    <div class="blank-space1" />
-    <div class="content-1 my-5">
-      <div class="row">
-        <div class="col-12 col-lg-7 col-md-6 col-sm-12">
-          <h1 class="mb-1">
-            Hi! Im DwiiUnknown
-          </h1>
-          <p class="lead">
-            My name is Satya Bagus Dwiatmaja. Im Frontend Web Developer, love to make nice websites and discord bots.
-          </p>
+  <div class="main">
+    <div class="content-1">
+      <div class="blank-space3" />
+      <div class="container">
+        <div class="row">
+          <div class="col-12 col-lg-7 col-md-6 col-sm-12">
+            <h1 class="mb-2">
+              Hi! Im DwiiUnknown
+            </h1>
+            <p class="lead">
+              My name is Satya Bagus Dwiatmaja. Im Frontend Web Developer, love to make nice websites and discord bots.
+            </p>
+          </div>
         </div>
       </div>
+      <div class="blank-space3" />
     </div>
-    <div class="blank-space1" />
-    <div class="content-2 my-5">
-      B
+
+    <div class="content-2">
+      <div class="blank-space3" />
+      <div class="container">
+        <h2 class="mb-2">
+          About Me
+        </h2>
+        <p>
+          A
+        </p>
+      </div>
+      <div class="blank-space3" />
     </div>
-    <div class="blank-space1" />
-    <div class="content-3 my-5">
-      C
+
+    <div class="content-3">
+      <div class="blank-space3" />
+      <div class="container">
+        <h2 class="mb-2">
+          My Projects
+        </h2>
+        <p>
+          A
+        </p>
+      </div>
+      <div class="blank-space3" />
     </div>
-    <div class="blank-space1" />
+
+    <div class="content-4">
+      <div class="blank-space3" />
+      <div class="container">
+        <h2 class="mb-2">
+          Social Media
+        </h2>
+        <p>
+          A
+        </p>
+      </div>
+      <div class="blank-space3" />
+    </div>
   </div>
 </template>
