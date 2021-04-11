80c0470102999e975679bb7beb48d2783625e37d
ItzMeDwii <dwiiunknown@gmail.com>
2021-04-11 18:27:03 +0700

    add content-2

--- pages/index.vue
+++ pages/index.vue
@@ -40,7 +40,12 @@
           About Me
         </h2>
         <p>
-          {{ lipsum }}
+          My name is Satya Bagus Dwiatmaja, Im 16 years old. Now im a High School Student. I live in East Java, Indonesia.<br>
+          I can make website using Nuxt.js (Vue.js Framework).<br>
+          I already making websites since 2018, i also making discord bots using JavaScript.<br>
+          <br>
+          What else I can do:<br>
+          HTML, CSS, PHP, JavaScript, TypeScript, React, NGINX, System Administration (Linux) and a little bit of UI/UX.
         </p>
       </div>
       <div class="blank-space4" />
@@ -52,9 +57,25 @@
         <h2 class="mb-3">
           My Projects
         </h2>
-        <p>
-          {{ lipsum }}
+        <p class="mb-3">
+          Here is some project that i made & i have contributed.
         </p>
+        <div class="row">
+          <div v-for="project of projects" :key="project.slug" class="col-6 col-lg-3 col-md-4 mb-4">
+            <div class="card">
+              <img :src="project.img" class="card-img-top" :alt="project.title">
+              <div class="card-body">
+                <h5 class="card-title fw-bold">
+                  {{ project.title }}
+                </h5>
+                <p class="card-text">
+                  {{ project.description }}
+                </p>
+                <a :href="project.link" class="btn">Visit Project</a>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
       <div class="blank-space4" />
     </div>
@@ -78,8 +99,37 @@
 export default {
   data () {
     const lipsum = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum interdum metus eget odio pretium, vel commodo nisi accumsan. Nulla egestas tortor eget dui rutrum, a consectetur enim tempus. Nullam nec lorem nec odio malesuada scelerisque at eget diam. Morbi tempus mi metus, nec faucibus urna venenatis ut. Proin commodo elit non dolor condimentum porttitor. Duis ipsum est, scelerisque sit amet sem ut, eleifend venenatis nibh. Aenean cursus cursus aliquam. Donec sollicitudin erat quam, quis consectetur mauris iaculis ut. Phasellus finibus justo vitae hendrerit elementum. Pellentesque condimentum lacus at lorem scelerisque accumsan. Etiam scelerisque ipsum a sodales hendrerit. Donec pharetra augue eget sollicitudin vulputate.'
-
-    return { lipsum }
+    const projects = [
+      {
+        id: '0',
+        title: 'My Website',
+        description: 'Nuxt.js',
+        img: '/preview/dwii.png',
+        link: 'https://dwii.my.id'
+      },
+      {
+        id: '1',
+        title: 'NGANTRIIN',
+        description: 'Nuxt.js',
+        img: '/preview/ngantriin.png',
+        link: 'https://ngantriin.com'
+      },
+      {
+        id: '2',
+        title: 'Auron Empire',
+        description: 'Nuxt.js',
+        img: '/preview/auron.png',
+        link: 'https://auronempire.com'
+      },
+      {
+        id: '3',
+        title: 'Animania.gg',
+        description: 'Nuxt.js',
+        img: '/preview/animania.png',
+        link: 'https:/animania.gg'
+      }
+    ]
+    return { lipsum, projects }
   },
   methods: {
     toTop () {
